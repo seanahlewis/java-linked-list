e7eb263bd83e3396051af7409a2fc163d209c75d
Seanah <145713180+seanahlewis@users.noreply.github.com>
2026-04-27 12:17:39 -0500

    Add files via upload
    Initial upload of linked list implementation

---
+++ Item.java
@@ -0,0 +1,39 @@
+package Assignment10;
+
+public class Item {
+	
+	private String name;
+	private int quantity;
+	private double price;
+	
+	public Item(String name, int quantity, double price) {
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	// Getters & Setters
+	public String getName() { return name; }
+	
+	public int getQuantity() { return quantity; }
+	
+	public double getPrice() { return price; }
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+	    return name + " | Qty: " + quantity + " | Price: " + String.format("%.2f", price);
+	}
+
+}
